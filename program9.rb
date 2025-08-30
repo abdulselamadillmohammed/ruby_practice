f06f14d7c60a0645aa6e9c5d4cffe234162c9a18
abdulselamadillmohammed <forcoding39@gmail.com>
2025-08-30 11:30:20 -0400

    update program9

--- program9.rb
+++ program9.rb
@@ -74,3 +74,21 @@ else
 end
 
 # --- for loop --- 
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+for friend in friends
+    puts friend
+end
+
+friends.each do |friend|
+    puts friend
+end
+
+# range of nums
+for index in 0..5
+    puts index
+end
+
+6.times do |index|
+    puts index
+end
