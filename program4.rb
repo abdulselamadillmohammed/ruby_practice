f7941694ac191f0a9f2877f8ea4924b5bdac3ded
abdulselamadillmohammed <forcoding39@gmail.com>
2025-08-30 08:51:03 -0400

    string methods

---
+++ program4.rb
@@ -0,0 +1,17 @@
+phrase = "Giraffe\" Academy      "
+puts phrase.upcase()
+
+puts phrase.downcase()
+
+puts phrase.lstrip()
+puts phrase.strip()
+puts phrase.rstrip()
+
+puts phrase.length()
+
+ 
+puts phrase.include?("Giraffe")
+
+puts phrase[2,4] # like python has exclusive end pos
+
+puts phrase.index("A")
