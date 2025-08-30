7d1003bab91fe277311fca788dcc011dcb690565
abdulselamadillmohammed <forcoding39@gmail.com>
2025-08-30 09:48:29 -0400

    arrays

---
+++ program7.rb
@@ -0,0 +1,10 @@
+friends = Array["Kevin", "Karen", "Oscar"]
+friends[0] = "Dwight"
+puts friends[0]
+puts friends.length()
+puts friends.reverse()
+puts friends.sort()
+new_arr = Array.new
+new_arr[0] = "1"
+new_arr[5] = "6"
+puts new_arr[4]
