08fadc855f7d54cc57a19b0df77ab5a50df75adf
abdulselamadillmohammed <forcoding39@gmail.com>
2025-08-30 10:17:48 -0400

    max func

--- program8.rb
+++ program8.rb
@@ -12,3 +12,33 @@ def sayhi(name, age=-1)
 end
 
 sayhi("Mike", 73)
+
+def cube(num)
+    return num * num * num, 70
+end
+
+#puts cube(3)[1]
+
+# --- If statements ---
+
+ismale = false
+
+if ismale
+    puts "You are male"
+else
+    puts "You are not males"
+end
+# and, or, not are supported
+# elsif instead of elseif or elif
+
+def max(num1, num2, num3)
+    if num1 >= num2 and num1 >=num3
+        return num1
+    elsif num2 >= num1 and num2 >= num3
+        return num2
+    else 
+        return num3
+    end
+end
+
+puts max(1,2,3)
